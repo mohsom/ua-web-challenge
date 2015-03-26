1bc9d65f42d2ea42dd7d37f426b32b247ad11ca0
mohsom <volodya.skalskyi@gmail.com>
2015-03-26 12:26:18 +0200

    Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,34 @@
+module.exports = function (grunt) {
 
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'index.html': 'build/index.html',
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'build/styles/styles.css': ['styles/style.css','styles/reset.css','styles/normalize.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.registerTask('default', ['cssmin', 'htmlmin', 'imagemin']);
+};
