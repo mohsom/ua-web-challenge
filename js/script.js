645812925f6641ace362942565901f6534637150
mohsom <volodya.skalskyi@gmail.com>
2015-03-27 16:24:49 +0200

    Adaptive CSS.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function () {
+    var click=0;
+    $('.show-menu').click(function () {
+        if(click===1){
+        $('.navbar-item-link').removeClass('active-menu');
+        click=0;
+        }
+        else{
+            $('.navbar-item-link').addClass('active-menu');
+            click=1;
+        }
+    });
+});
