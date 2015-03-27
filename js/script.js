0aa25071bed34bf5932f3e4e83446b4bddefec08
mohsom <volodya.skalskyi@gmail.com>
2015-03-27 16:28:58 +0200

    Adaptive CSS.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
-    var click=0;
+    var click = 0;
     $('.show-menu').click(function () {
-        if(click===1){
-        $('.navbar-item-link').removeClass('active-menu');
-        click=0;
+        if (click === 1) {
+            $('.navbar-item-link').removeClass('active-menu');
+            $('.show-menu').removeClass('active-arrow');
+            click = 0;
         }
-        else{
+        else {
             $('.navbar-item-link').addClass('active-menu');
-            click=1;
+            $('.show-menu').addClass('active-arrow');
+            click = 1;
         }
     });
 });
