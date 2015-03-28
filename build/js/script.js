431a96459a6f83a7308141ad17c769ec09569d22
mohsom <volodya.skalskyi@gmail.com>
2015-03-28 18:22:33 +0200

    Making code cleaner.

---
+++ build/js/script.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){var a=0;$(".show-menu").click(function(){1===a?($(".navbar-item-link").removeClass("active-menu"),$(".show-menu").removeClass("active-arrow"),a=0):($(".navbar-item-link").addClass("active-menu"),$(".show-menu").addClass("active-arrow"),a=1)})});
